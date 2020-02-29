921690108c70a3731b9416cbde5972f10993f91b
NsMate <sandorm.mate@gmail.com>
2020-02-29 21:30:10 +0100

    Made the routing, made the login form and added the service which will be responsible for the authorization

--- business-frontend/src/app/app-routing.module.ts
+++ business-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginFormComponent } from './login-form/login-form.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
