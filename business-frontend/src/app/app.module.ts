0c77429fc2d65aa697e2de6b042d765eb308be7e
NsMate <sandorm.mate@gmail.com>
2020-03-23 19:10:43 +0100

    Changed title of app, added modules to app module ts

--- business-frontend/src/app/app.module.ts
+++ business-frontend/src/app/app.module.ts
@@ -15,11 +15,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { VehicleListComponent } from './components/ListComponents/VehicleList/vehicle-list/vehicle-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    VehicleListComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +40,11 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    MatInputModule
+    MatInputModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
